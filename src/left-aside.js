a694148abf75887baafeb10c23445ba83b9661a5
Joe Mordetsky <jmordetsky@bloomberg.net>
2015-10-03 06:01:41 -0400

    Cosmetic update

--- src/left-aside.js
+++ src/left-aside.js
@@ -5,13 +5,13 @@ var LeftAside = React.createClass({
         return (
             <aside className="left-stuff">
                 <div className="header">
-                    <div class="side-bar">
+                    <div className="supporting-text side-bar">
                             <h3>Javascript enhanced cyber primate</h3>
                             <p>by j03m</p>
 
-                            <a href="http://twitter.com/j03m" class="social" target="_blank"><i class="fa fa-twitter-square"></i> Twitter</a>
+                            <a href="http://twitter.com/j03m" class="social" target="_blank"><i className="fa fa-twitter"></i> Twitter</a>
                             <br/>
-                            <a href="http://github.com/j03m" class="social" target="_blank"><i class="fa fa-github"></i> GitHub</a>
+                            <a href="http://github.com/j03m" class="social" target="_blank"><i className="fa fa-github"></i> GitHub</a>
 
                     </div>
                 </div>
